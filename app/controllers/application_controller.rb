a1ef8cc49ad07967936c9fd98a815aa841ed8aa4
colinfong <colinjoefong@gmail.com>
2018-04-25 20:41:56 -0700

    Added postgres 0.20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
+    render html: "hey May! I created a basic website!"
   end
 end
